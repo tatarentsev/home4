090cb9673b13bfca89687a6e03e5c5531c50b4a5
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-01-28 00:52:51 +0300

    Third #27

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+
+// while (num > 0)
+// {
+//     count += num % 10;
+//     num /= 10;
+// }
+
+// Console.WriteLine(count);
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (num > 0)
+{
+    int last_num = num % 10;
+    count += last_num;
+    num /= 10;
+}
+
+Console.WriteLine(count);
